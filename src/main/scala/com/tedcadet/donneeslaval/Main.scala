c3d8534df3c85186eb1ca432f78bdfa41de1fee4
TedCadet <e_cadet22@hotmail.com>
2022-12-18 22:08:11 -0500

    queries concerning the nature of the contracts added

--- src/main/scala/com/tedcadet/donneeslaval/Main.scala
+++ src/main/scala/com/tedcadet/donneeslaval/Main.scala
@@ -1,16 +1,16 @@
 package com.tedcadet.donneeslaval
 
-import com.tedcadet.donneeslaval.contrats.ContratQueries.{listeContractantsQueries, montantCumulatifParContractantsQuery, montantDepenseParAnneeQuery}
+import com.tedcadet.donneeslaval.contrats.ContratQueries.{listeContractantParNatureQuery, listeContractantsQuery, listeNaturesDeContratQuery, listeParColonneDistinctQuery, montantCumulatifParContractantsQuery, montantDepenseParAnneeQuery, montantOctroyeParNaturesQuery}
+import com.tedcadet.donneeslaval.contrats.modeles.Contractant
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Main extends App {
 
-  case class Contractant(contractant: String)
   // TODO: peut etre externaliser dans un fichier conf
   // creation du sparkSession
   val sparkSession: SparkSession = SparkSession.builder
     .appName("file-json-processing-test")
-    .master("local[1]")
+    .master("local[2]")
     .getOrCreate()
 
   import sparkSession.implicits._
@@ -27,7 +27,7 @@ object Main extends App {
 //  contrats.printSchema()
 
   //liste des contractants
-  val listeContractants: DataFrame = listeContractantsQueries(contrats)
+  val listeContractants: DataFrame = listeContractantsQuery(contrats)
 
   // montant cumulatif pour chacun des contractants
   val montantCumulatifParContractants: DataFrame = montantCumulatifParContractantsQuery(contrats)
@@ -35,12 +35,32 @@ object Main extends App {
   // argent depensee par annee
   val montantDepenseParAnnee: DataFrame = montantDepenseParAnneeQuery(contrats)
 
+  // liste des natures de contrats
+  val listeDesNaturesDeContrat: DataFrame = listeNaturesDeContratQuery(contrats)
+
+  // argent depensee par nature du contrat
+  val montantOctroyeParNatures: DataFrame = montantOctroyeParNaturesQuery(contrats)
+
+  // liste de contrat selon leur nature
+  val listeContractantParNature: DataFrame = listeContractantParNatureQuery(contrats, "Acquisition")
+
+
+
   // show les resultats
+//  listeParColonneDistinctQuery(contrats,colNoReference).show()
   listeContractants.show()
   montantCumulatifParContractants.show()
   montantDepenseParAnnee.show()
-  
-  val contractant = listeContractants.as[Contractant].first().toString
+  listeDesNaturesDeContrat.show()
+  montantOctroyeParNatures.show()
+  listeContractantParNature.show()
+  println(s"nombre de contrats: ${contrats.count()}")
+
+
+  montantCumulatifParContractants.printSchema()
+  montantDepenseParAnnee.printSchema()
+
+  val contractants = listeContractants.as[Contractant].first().toString
 
   // fermeture de la session
   sparkSession.close()
--- src/main/scala/com/tedcadet/donneeslaval/contrats/ContratQueries.scala
+++ src/main/scala/com/tedcadet/donneeslaval/contrats/ContratQueries.scala
@@ -1,19 +1,29 @@
 package com.tedcadet.donneeslaval.contrats
 
-import com.tedcadet.donneeslaval.contrats.Columns.{colAnne, colContractant, colDate, colMontant}
+import com.tedcadet.donneeslaval.contrats.Columns.{colAnne, colBigMontant, colContractant, colDate, colMontant, colNature}
 import com.tedcadet.donneeslaval.contrats.ContratUserDefinedFunctions.toBigDecimal
-import org.apache.spark.sql.{DataFrame, functions}
+import org.apache.spark.sql.{Column, DataFrame, functions}
 import org.apache.spark.sql.functions.col
 
 object ContratQueries {
 
   /**
+   *
+   * @param contratsDF liste des contrats dans un dataFrame
+   * @param colonne colonne qu'on veut recuperer
+   * @return liste des elements distincts d'une colonne
+   */
+  def listeParColonneDistinctQuery(contratsDF: DataFrame, colonne: Column): DataFrame =
+    contratsDF.select(colonne).distinct
+
+  /**
    * @param contratsDF liste des contrats dans un dataFrame
    * @return la liste des contractants
    */
-  def listeContractantsQueries(contratsDF: DataFrame): DataFrame =
+  def listeContractantsQuery(contratsDF: DataFrame): DataFrame =
     contratsDF.select(colContractant).distinct
 
+  //TODO: passe comme parametre le type de contractant. Pour l'instant il recherche que les contractants en construction
   /**
    * @param contratsDF liste des contrats dans un dataFrame
    * @return le montant cumulatif par contractants
@@ -21,11 +31,9 @@ object ContratQueries {
   def montantCumulatifParContractantsQuery(contratsDF: DataFrame): DataFrame =
     contratsDF
       .select(colContractant, colMontant)
-      //TODO: voir comment remplacer l'expression en utilisant directement la colonne
-      .where(" nature like '%construction%'")
       .groupBy(colContractant)
-      .agg(functions.sum(colMontant).as("montant cumulatif(par consultant)"))
-      .orderBy(toBigDecimal(col("montant cumulatif(par consultant)")).desc)
+      .agg(functions.sum(colBigMontant).as("montant cumulatif(par consultant)"))
+      .orderBy(col("montant cumulatif(par consultant)").desc)
 
   /**
    *
@@ -35,7 +43,36 @@ object ContratQueries {
   def montantDepenseParAnneeQuery(contratsDF: DataFrame): DataFrame =
     contratsDF
       .select(colDate, colMontant)
-      .groupBy(colAnne)
-      .agg(functions.sum(colMontant).as("montant cumulatif(par annee)"))
+      .groupBy(colAnne.as("annee"))
+      .agg(functions.sum(colBigMontant).as("montant cumulatif(par annee)"))
       .orderBy(colAnne)
+
+  /**
+   *
+   * @param contratsDF liste des contrats dans un dataFrame
+   * @return la liste des natures de contrat
+   */
+  def listeNaturesDeContratQuery(contratsDF: DataFrame): DataFrame =
+    contratsDF
+      .select(colNature)
+      .distinct()
+
+  /**
+   *
+   * @param contratDF liste des contrats dans un dataFrame
+   * @return le montant depense par nature de contrats
+   */
+  def montantOctroyeParNaturesQuery(contratDF: DataFrame): DataFrame =
+    contratDF
+      .select(colNature, colMontant)
+      .groupBy(colNature.as("nature du contrat"))
+      .agg(functions.sum(colBigMontant).as("montant cumulatif(par nature du contrat)"))
+      .orderBy(col("montant cumulatif(par nature du contrat)").desc)
+
+  def listeContractantParNatureQuery(contratsDF: DataFrame, nature: String): DataFrame =
+    contratsDF
+      .select("*")
+      //TODO: Security Risk SQL injection: voir comment remplacer sans passer directement la nature
+      .where(s" nature like '%${nature}%'")
+
 }
