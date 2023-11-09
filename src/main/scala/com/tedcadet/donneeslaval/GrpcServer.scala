44aefc8a0a5fb677114cd358b6a2a2d60dd1ba14
TedCadet <e_cadet22@hotmail.com>
2023-11-08 21:57:40 -0500

    the ActorSystem used is now from akka.actor.typed.ActorSystem

--- src/main/scala/com/tedcadet/donneeslaval/GrpcServer.scala
+++ src/main/scala/com/tedcadet/donneeslaval/GrpcServer.scala
@@ -1,6 +1,8 @@
 package com.tedcadet.donneeslaval
 
-import akka.actor.ActorSystem
+import akka.NotUsed
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import com.tedcadet.donneeslaval.contrats.ContratServiceHandler
@@ -12,22 +14,22 @@ import scala.concurrent.{ExecutionContext, Future}
 object GrpcServer extends App {
   val conf = ConfigFactory.load()
 
-  val system = ActorSystem("contrats-octroye-laval", conf)
+  val system = ActorSystem[NotUsed](Behaviors.empty, "contrats-octroye-laval", conf)
   new GrpcServer(system).run()
 }
 
-class GrpcServer(system: ActorSystem) {
+class GrpcServer(system: ActorSystem[NotUsed]) {
   def run(): Future[Http.ServerBinding] = {
     // Akka boot up code
-    implicit val sys: ActorSystem = system
-    implicit val ec: ExecutionContext = sys.dispatcher
+    implicit val sys: ActorSystem[NotUsed] = system
+    implicit val ec: ExecutionContext = sys.executionContext
 
     // create service handlers
     val service: HttpRequest => Future[HttpResponse] =
       ContratServiceHandler(new ContratServiceGrpcImpl())
 
     // Bind service handler servers to localhost:8080/8081
-    val host = "0.0.0.0"
+    val host = "127.0.0.1"
     val port = 8080
     val binding = Http().newServerAt(host, port).bind(service)
 
